07a85048f5b75e9ff15e879af808ad9c04e6505b
dcsiona@gmail.com <dcsiona@gmail.com>
2024-02-20 16:06:21 -0500

    commit for my messages.

--- phase-2.js
+++ phase-2.js
@@ -3,12 +3,19 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
+  for (let i = 0; i < 10; i++) {
+    addNums(i);
+  }
+
 
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
+  for (let i = 0; i < 10; i++) {
+    addManyNums(i);
+  }
 
 }
 
